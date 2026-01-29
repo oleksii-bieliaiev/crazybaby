e6dc05b8b74f22baa65fe48b02d72030b9c69020
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2026-01-30 00:17:57 +0100

    commit 30/01/26

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,25 +4,25 @@ import Image from 'next/image'
 
 const Header = () => {
   return (
-    <section id='header' className='relative flex flex-col lg:min-h-auto max-sm:min-h-auto w-screen lg:mb-10 md:mb-30 sm:mb-20 max-sm:mb-40'>
-      <div className='grid lg:grid-cols-12 2xl:gap-8 lg:gap-x-0 lg:gap-y-0 md:grid-cols-8 sm:grid-cols-6 max-sm:grid-cols-2 lg:pr-[123px] lg:pl-[123px] md:pr-[34px] md:pl-[34px] sm:pl-[34px] max-sm:pl-0 sm:pr-[34px] max-sm:pr-0  md:w-auto md:mx-0 sm:w-full sm:mx-auto max-sm:w-full max-sm:mx-auto  '>
+    <section id='header' className='relative flex flex-col lg:min-h-auto max-sm:h-auto w-screen  lg:mb-10 md:mb-30 sm:mb-20 max-sm:mb-40'>
+      <div className='grid lg:grid-cols-12 2xl:gap-8 lg:gap-x-0 lg:gap-y-0 md:grid-cols-8 sm:grid-cols-6 max-sm:grid-cols-4 lg:pr-[123px] lg:pl-[123px] md:pr-[34px] md:pl-[34px]  md:w-auto md:mx-0 sm:w-full sm:mx-auto max-sm:w-full max-sm:mx-auto  '>
         <NavBar />
-        <div className='lg:col-start-1 lg:col-span-3 md:col-start-1 md:col-span-3 z-50 lg:mt-15 text-[#333333]'>
-          <div className='font-sans lg:text-[62px] md:text-[48px] lg:leading-[78px] md:leading-[62px] md:tracking-[0] mb-8'>
+        <div className='lg:col-start-1 lg:col-span-3 md:col-start-1 md:col-span-3 max-sm:col-start-1 max-sm:col-span-4 z-50 lg:mt-15 text-[#333333] max-sm:items-center max-sm:flex max-sm:flex-col max-sm:order-2 max-sm:pl-[30px] max-sm:pr-[30px] '>
+          <div className='font-sans lg:text-[62px] md:text-[48px] max-sm:text-[40px] lg:leading-[78px] md:leading-[62px] max-sm:leading-[52px] md:tracking-[0] mb-8'>
             <h1 className='uppercase font-bold'>Futuristic Wireless Speaker</h1>
           </div>
           <div className='font-sans md:text-[16px] md:leading-[24px] md:tracking-[0]'>
-            <p className='font-normal w-[260px]'>
+            <p className='font-normal md:w-[260px] max-sm:items-center max-sm:flex '>
               Luna’s performance is balanced and smooth in all frequency ranges which makes the music both naturally pleasant and distinctly more layered.
             </p>
           </div>
         </div>
-        <div className='lg:col-start-5 lg:col-span-8 md:col-start-4 md:col-span-5 lg:mt-0 md:mt-3'>
+        <div className='lg:col-start-5 lg:col-span-8 md:col-start-4 md:col-span-5 max-sm:col-start-1 max-sm:col-span-4 lg:mt-0 md:mt-3 max-sm:order-1 max-sm:mb-12 max-sm:translate-x-2'>
           <Image src="/images/img_desktop.svg"
             alt="img_desktop"
             width={816}
             height={464}
-            className='object-cover object-left lg:w-[816px] lg:h-[464px] md:w-[430px] md:h-[317px] justify-center'
+            className='object-cover object-left lg:w-[816px] lg:h-[464px] md:w-[430px] md:h-[317px] max-sm:w-full max-sm:h-[250px] justify-center max-sm:items-center'
           />
         </div>
       </div>
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 
 const NavBar = () => {
   return (
-    <div className='flex col-start-1 col-span-12 2xl:mb-10 lg:mb-17 md:mb-27 mt-7'>
-      <div className='flex w-full h-full justify-between items-center z-20 lg:pt-0 2xl:pb-0 2xl:mt-0 lg:pb-0 md:pt-0 sm:pt-6 md:pb-0 sm:pb-12 max-sm:pb-0 max-sm:pt-6 max-sm:w-full '>
+    <div className='flex col-start-1 col-span-12 2xl:mb-10 lg:mb-17 md:mb-27 max-sm:mb-7 mt-7 max-sm:pl-[30px] max-sm:pr-[30px]'>
+      <div className='flex w-full h-full justify-between items-center z-20 lg:pt-0 2xl:pb-0 2xl:mt-0 lg:pb-0 md:pt-0 sm:pt-6 md:pb-0 sm:pb-12 max-sm:pb-0 max-sm:pt-0 max-sm:w-full '>
         <Image
           src="/images/logo.svg"
           alt="Company Logo"
@@ -16,7 +16,7 @@ const NavBar = () => {
 
         <div className='text-[#4F4F4F] lg:gap-15 md:gap-7 flex items-center'>
           <a href='#features' className="
-            uppercase relative cursor-pointer text-[14px] font-normal leading-[100%]
+            uppercase relative cursor-pointer max-sm:hidden text-[14px] font-normal leading-[100%]
             transition-all duration-300
           hover:text-[#131313]
             hover:drop-shadow-[0_1px_2px_rgba(0,0,0,0.7)]
@@ -24,7 +24,7 @@ const NavBar = () => {
             specs
           </a>
           <a href='#features' className="
-            uppercase relative cursor-pointer text-[14px] font-normal leading-[100%]
+            uppercase relative cursor-pointer max-sm:hidden text-[14px] font-normal leading-[100%]
             transition-all duration-300
           hover:text-[#131313]
             hover:drop-shadow-[0_1px_2px_rgba(0,0,0,0.7)]
@@ -33,7 +33,7 @@ const NavBar = () => {
           </a>
 
           <button
-            className=" md:w-[100px] sm:w-[240px] max-sm:w-full h-[48px] bg-[#333333] hover:bg-[#131313] tracking-[0] cursor-pointer font-semibold text-[12px] text-[#FFFFFF]  flex items-center justify-center gap-2 transition-colors duration-300">
+            className=" md:w-[100px] sm:w-[240px] max-sm:hidden h-[48px] bg-[#333333] hover:bg-[#131313] tracking-[0] cursor-pointer font-semibold text-[12px] text-[#FFFFFF]  flex items-center justify-center gap-2 transition-colors duration-300">
             BUY
           </button>
 
